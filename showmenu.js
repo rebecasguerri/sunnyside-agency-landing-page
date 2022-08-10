3e24838f2cc50e1b3355915d2f7c3612d9453363
rebecasguerri <rebecasguerri@gmail.com>
2022-08-10 11:44:12 -0300

    update

--- showmenu.js
+++ showmenu.js
@@ -3,5 +3,6 @@ let navMenu = document.querySelector(".nav-menu");
 
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle('active');
-   
+    navMenu.classList.toggle('active');
+
 });
